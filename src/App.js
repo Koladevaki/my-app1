7ae2251e319388223f46aa9ea4b63efc4cfa46fd
sowmya <koladevaki5@gmail.com>
2024-10-18 09:26:34 +0530

    changed my name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>sowmya</h1>
+       <h1>sowmya yadav</h1>
       </header>
     </div>
   );
